4e30c48d6e92e726b391801909ba87af6eca71fc
Chloroauric Acid <70007256+Aeroraven@users.noreply.github.com>
2023-11-05 13:55:57 +0800

    Prepare for the next step

---
+++ Anthem/demo/AD10_ComputeShader.cpp
@@ -0,0 +1,16 @@
+#include "../include/core/renderer/AnthemSimpleToyRenderer.h"
+#include "../include/core/math/AnthemMathAbbrs.h"
+#include "../include/external/AnthemGLTFLoader.h"
+#include "../include/external/AnthemImageLoader.h"
+#include "../include/components/camera/AnthemCamera.h"
+
+using namespace Anthem::Core;
+using namespace Anthem::Core::Math;
+using namespace Anthem::Core::Math::Abbr;
+using namespace Anthem::External;
+using namespace Anthem::Components::Camera;
+
+int main(){
+    ANTH_LOGI("Meow!");
+    return 0;
+}
